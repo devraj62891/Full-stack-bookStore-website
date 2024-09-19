11bce0709821507c672320c36ef5ad1a2a065d8f
DEVRAJ SHARMA <70874547+devraj62891@users.noreply.github.com>
2024-09-19 19:31:09 +0000

    .

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,5 @@
 import express, { urlencoded } from "express";
 import cors from "cors"
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 
 const app=express();
@@ -13,15 +11,6 @@ app.use(express.json());
 // app.use(express.static("public"))
 
 // Serve static files from the React frontend app
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// Anything that doesn't match the API routes should be passed to the React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
 
 
 export {app};
