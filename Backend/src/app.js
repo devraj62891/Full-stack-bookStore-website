7211db87e9c5f44390f38fb9ec5f112be414cb49
DEVRAJ SHARMA <70874547+devraj62891@users.noreply.github.com>
2024-09-19 18:25:08 +0000

    added cors in the app.js for deployement

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,15 +3,8 @@ import cors from "cors"
 
 const app=express();
 
-// app.use(cors());
-app.use(cors(
-    {
-        origin:["https://deploy-mern-1whq.vercel.app"],
-        methods:["POST","GET"],
-        credentials:true
+app.use(cors());
 
-    }
-))
 app.use(express.json());
 // app.use(urlencoded({extended:true}))
 // app.use(express.static("public"))
