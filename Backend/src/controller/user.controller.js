acc13faf486a7d29b8938f3abe835085df6cce86
DEVRAJ SHARMA <70874547+devraj62891@users.noreply.github.com>
2024-09-19 21:12:15 +0000

    fixed wrong toast configurationof login

--- Backend/src/controller/user.controller.js
+++ Backend/src/controller/user.controller.js
@@ -32,10 +32,15 @@ export const login = async(req, res) => {
     try {
         const { email, password } = req.body;
         const user = await User.findOne({ email });
-        const isMatch = await bcryptjs.compare(password, user.password);
-        if (!user || !isMatch) {
-            return res.status(400).json({ message: "Invalid username or password" });
+        if (!user ) {
+            return res.status(400).json({ message: "user does not exist" });
         } else {
+        const isMatch = await bcryptjs.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({ message: "wrong username or password" });
+
+        }
+
             res.status(200).json({
                 message: "Login successful",
                 user: {
